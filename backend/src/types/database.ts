0d40f7b10df638eeb820f1a311f93f9f934f11ec
momboarmellevicky-droid <momboarmellevicky@gmail.com>
2026-07-29 02:40:22 +0100

    Create database.ts

---
+++ backend/src/types/database.ts
@@ -0,0 +1,8 @@
+export type Database = {
+  public: {
+    Tables: Record<string, any>;
+    Views: Record<string, any>;
+    Functions: Record<string, any>;
+    Enums: Record<string, any>;
+  };
+};
